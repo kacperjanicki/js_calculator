1a60af2ecfebe222bb596be593ca8ba894ec936a
kacperjanicki <kacperekjanicki9@gmail.com>
2022-06-07 20:30:24 +0200

    all works

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ elements.forEach(element => element.addEventListener('click',mainfunc))
 equation=""
 
 function mainfunc() {
+    if (this.innerHTML == '÷') {
+        this.textContent = '/'
+    }
     if (this.innerHTML == '=') {
         display.innerHTML = `${eval(equation)}`
         console.log(eval(equation))
+        equation = ''
+    }
+    else if (this.innerHTML == '+/-') {
+        equation=eval(equation) * -1
+        display.innerHTML = equation
     }
     else if (this.innerHTML == 'AC') {
         display.innerHTML = ''
+        equation = '' }
+    else if (this.innerHTML == '√') {
+        equation = `${eval(equation)**(1/2)}`
+        display.innerHTML = equation
         equation = ''
 
-    } else {
+    }
+    else {
         if (Number(this.innerHTML)) {   //if number clicked
             equation+=this.textContent
             display.innerHTML = equation
